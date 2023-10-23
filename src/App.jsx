7149ab9e57e1e8dcb9312800af51ab17eddb5619
olivia <olicivit@gmail.com>
2023-10-23 19:04:13 -0300

    cc

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './output.css';
 import Navbar from './components/Navbar.jsx'
 import ItemListContainer from './components/ItemListContainer';
+import ItemCount from './components/ItemCount'
+
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
 			<ItemListContainer/>
+			
 		</div>
 	);
 }
 
 export default App;
+
