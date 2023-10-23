92fc821188910d54e7594813bc530c59b0002cb7
olivia <olicivit@gmail.com>
2023-10-23 19:13:13 -0300

    ccc

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './output.css';
 import Navbar from './components/Navbar.jsx'
 import ItemListContainer from './components/ItemListContainer';
-import ItemCount from './components/ItemCount'
+import ItemCount from './components/ItemCount';
 
 
 function App() {
@@ -9,10 +9,13 @@ function App() {
 		<div className="App">
 			<Navbar />
 			<ItemListContainer/>
-			
+			<ItemCount initial={1} stock={10} onAdd={(quantity) => console.log("Cantidad agregada", quantity)} />
 		</div>
 	);
 }
+
+	
+
 
 export default App;
 
